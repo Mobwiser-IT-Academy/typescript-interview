b0c625dac0d7ecd68ed9abd340a350a3fa609396
Fábio Rito <fabioaarito@MBP-de-Fabio.lan>
2020-11-06 15:10:35 +0000

    add question 3 and readme

---
+++ src/question-3/index.ts
@@ -0,0 +1,119 @@
+/**
+ * Supermarket challenge
+ *
+ * Build the following objects
+ *  - Product
+ *  - Bag
+ *  - Transaction
+ *  - Supermarket
+ *
+ * A "Product" has two attributes ("name" and "price")
+ *  - Add a constructor that received these two attributes
+ *
+ *
+ * A "Bag" has one attribute ("products") that is an array of products.
+ * Add the following methods to the Bag object:
+ *  - "totalSpent" - this should be a getter and it should return the total price of all products existing in the bag.
+ *  - "add" - add a new product to the bag
+ *
+ *
+ * A "Transaction" contains two attributes:
+ *  - "bag" - a bag;
+ *  - "time" - a datetime when the transaction was made;
+ *
+ * Add a constructor that received only a bag. the time should be the date time when the transaction is created.
+ *
+ *
+ * The "Supermarket"
+ *  - "transactions" - stores a list of transactions
+ *  - "limitWithoutDiscount" - the limit spent where the user won't have any discount
+ *  - "discountPercentage" - a discount percentage if the "total spent" is greater than the "limitWithoutDiscount"
+ *
+ * Add a constructor that receives the limitWithoutDiscount and the discountPercentage.
+ * Add the following methods:
+ * - "registerTransaction" - this method should receive a bag and register a new transaction with that bag
+ * - "hasDiscount" - this method should receive a transaction and check whether the transactions had a discount or not
+ * - "totalDiscount" - this method should receive a transaction and check the total discount for that transaction
+ * - "numberOfTransactionsWithDiscount" - this should be a getter that returns the number of transactions without discount
+ * - "totalDiscountInSupermarket" - this should be a getter that returns the total discount applied in the supermarket
+ *
+ */
+
+export class Product {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+export class Bag {
+  products: Product[] = [];
+
+  public get totalSpent() {
+    return this.products.reduce((total: number, product: Product) => {
+      total += product.price;
+      return total;
+    }, 0);
+  }
+
+  public add(product: Product) {
+    this.products.push(product);
+  }
+}
+
+export class Transaction {
+  bag: Bag;
+  time: Date;
+
+  constructor(bag) {
+    this.bag = bag;
+    this.time = new Date();
+  }
+}
+
+export class Supermarket {
+  transactions: Transaction[] = [];
+  limitWithoutDiscount: number;
+  discountPercentage: number;
+  constructor(limitWithoutDiscount: number, discountPercentage: number) {
+    this.limitWithoutDiscount = limitWithoutDiscount;
+    this.discountPercentage = discountPercentage;
+  }
+
+  public registerTransaction(bag: Bag) {
+    this.transactions.push(new Transaction(bag));
+  }
+
+  public hasDiscount(transaction: Transaction): boolean {
+    return transaction.bag.totalSpent >= this.limitWithoutDiscount;
+  }
+
+  public totalDiscount(transaction: Transaction): number {
+    return this.hasDiscount(transaction)
+      ? transaction.bag.totalSpent * this.discountPercentage
+      : 0;
+  }
+
+  public get numberOfTransactionsWithDiscount() {
+    return this.transactions.reduce(
+      (total: number, transaction: Transaction) => {
+        if (this.hasDiscount(transaction)) {
+          total++;
+        }
+        return total;
+      },
+      0,
+    );
+  }
+
+  public get totalDiscountInSupermarket() {
+    return this.transactions.reduce(
+      (total: number, transaction: Transaction) =>
+        (total += this.totalDiscount(transaction)),
+      0,
+    );
+  }
+}
---
+++ test/question-3.test.ts
@@ -0,0 +1,72 @@
+import {
+  Supermarket,
+  Product,
+  Bag,
+  numberOfTransactionsWithDiscount,
+  totalDiscountInSupermarket,
+} from '../src/question-3';
+
+describe('validate question 3', () => {
+  let supermarket: Supermarket;
+  const soyMilk = new Product('Soy milk', 0.3);
+  const rice = new Product('Rice', 0.45);
+  const banana = new Product('Banana', 2);
+  const beans = new Product('Beans', 1);
+  const eggs = new Product('Eggs', 2.34);
+  const beer = new Product('Beer', 6);
+  const wine = new Product('Wine', 5);
+  beforeAll(() => {
+    const limitWithoutDiscount = 10;
+    const discountPercentage = 0.1;
+    supermarket = new Supermarket(limitWithoutDiscount, discountPercentage);
+
+    /**
+     * Total: 11,9
+     * Discount 1,19
+     */
+    const bag1 = new Bag();
+    addToBag(bag1, rice, 2);
+    addToBag(bag1, banana, 4);
+    addToBag(bag1, beans, 3);
+    supermarket.registerTransaction(bag1);
+
+    /**
+     * Total: 25,36
+     * Discount 2,536
+     */
+    const bag2 = new Bag();
+    addToBag(bag2, wine, 2);
+    addToBag(bag2, beer, 1);
+    addToBag(bag2, eggs, 4);
+    supermarket.registerTransaction(bag2);
+
+    /**
+     * Total: 0,6
+     * Discount 0
+     */
+    const bag3 = new Bag();
+    addToBag(bag3, soyMilk, 2);
+    supermarket.registerTransaction(bag3);
+
+    /**
+     * Total: 10
+     * Discount 1
+     */
+    const bag4 = new Bag();
+    addToBag(bag4, wine, 2);
+    supermarket.registerTransaction(bag4);
+  });
+  it('RQ0 - Check number of transactions with discount', () => {
+    expect(supermarket.numberOfTransactionsWithDiscount).toBe(3);
+  });
+
+  it('RQ1 - Check total discount in the supermarket', () => {
+    expect(supermarket.totalDiscountInSupermarket).toBe(4.726);
+  });
+
+  function addToBag(bag: Bag, product: Product, quantity: number) {
+    for (let i = 0; i < quantity; ++i) {
+      bag.add(product);
+    }
+  }
+});
