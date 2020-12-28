d8e33aeb9a5efe7cd0cfaf0ebc1614aec4ec698a
RitaSilva28-github <63984197+RitaSilva28@users.noreply.github.com>
2020-12-28 14:48:57 +0000

    questions 1 and 2

--- src/question-1/index.ts
+++ src/question-1/index.ts
@@ -1,3 +1,5 @@
+import { title } from "process";
+
 /**
  * Question 1 - Export a function called "analyseQuotes" that receives an array of quotes and
  * counts the number of counts by category.
@@ -15,6 +17,43 @@
  * console.log(analyseQuotes(quotes));
  * > {'Sports': 1, 'Politcs': 1, 'Motivational': 1}
  */
-interface Quote{
+export interface Quote  {
+
+     title:string;
+     category:string;
+
+  
+
+    };
+
+export function analyseQuotes(quotes:Quote[]): Record<string, number> 
+{
+
+    let objectResult={};
+    let quoteCategory= quotes;
+
+
+//for(let i = quotes.length-1; i>=0; i--){
+
+for(let i=0; i<quotes.length; i++){
+
+    objectResult[quotes[i].category] =+ 1;
+}
+
+return objectResult
+
 
 }
+
+
+
+
+
+
+    
+
+
+
+
+
+
