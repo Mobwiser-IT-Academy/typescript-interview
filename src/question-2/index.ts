c0c0af2b0defebeb59dd0f2137fa5d8244fd0b98
Bitkas <bitokeiro@gmail.com>
2021-02-11 10:12:48 +0000

    question 2

--- src/question-2/index.ts
+++ src/question-2/index.ts
@@ -15,3 +15,40 @@
  *  1 - Round all numbers to a 2 decimal places number.
  *  2 - You should create and export an enumerator called TemperatureUnit with three possible values: CELSIUS='Celsius', FAHRENHEIT='Fahrenheit', KELVIN='Kelvin'
  */
+
+import { type } from "os";
+
+ enum TemperatureUnit {
+    CELSIUS = 'Celsius',
+    FAHRENHEIT = 'Fahrenheit',
+    KELVIN = 'Kelvin'
+ };
+
+ function temperatureConversion(value : number, baseUnit : TemperatureUnit) : Record<string, number> {
+    let conversion : Record<string, number> = {
+        'Celsius' : 0,
+        'Fahrenheit' : 0,
+        'Kelvin' : 0
+    }
+    
+    switch (baseUnit) {
+        case TemperatureUnit.CELSIUS :
+            conversion.Celsius = +(value).toFixed(2);
+            conversion.Fahrenheit = +(value / 5 * 9 + 32).toFixed(2);
+            conversion.Kelvin = +(value + 273.15).toFixed(2);
+            break;
+        case TemperatureUnit.FAHRENHEIT : 
+            conversion.Celsius = +(( value - 32 ) * 5 / 9).toFixed(2);
+            conversion.Fahrenheit = +value.toFixed(2);
+            conversion.Kelvin = +(( value + 459.67 ) * ( 5 / 9 )).toFixed(2);
+            break;
+        case TemperatureUnit.KELVIN :
+                conversion.Celsius = +(value - 273.15).toFixed(2);
+                conversion.Fahrenheit = +(value * ( 9 / 5 ) - 459.67).toFixed(2);
+                conversion.Kelvin = +value.toFixed(2);
+            break;
+    }
+    return conversion;
+ }
+
+ export { TemperatureUnit, temperatureConversion };
